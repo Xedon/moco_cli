fa287c4a64317b8f2a0b7bac81ba281d790e28da
Rodolfo Kirch Veiga <rodolfoksveiga@gmail.com>
2022-06-06 12:51:39 +0200

    create function to select activity

--- src/main.rs
+++ src/main.rs
@@ -346,3 +346,56 @@ async fn promp_task_select(
 
     Ok((project.clone(), task.clone()))
 }
+
+async fn promp_activitie_select(
+    moco_client: &MocoClient,
+    activity: Option<i64>,
+) -> Result<moco::model::Activitie, Box<dyn Error>> {
+    let now = Utc::now().format("%Y-%m-%d").to_string();
+
+    print!("List activities from (YYYY-MM-DD) - Default 'today': ");
+    std::io::stdout().flush()?;
+
+    let mut from = utils::read_line()?;
+    if from.is_empty() {
+        from = now.clone();
+    }
+
+    print!("List activities to (YYYY-MM-DD) - Default 'last answer': ");
+    std::io::stdout().flush()?;
+
+    let mut to = utils::read_line()?;
+    if to.is_empty() {
+        to = from.clone();
+    }
+
+    let activities = moco_client.get_activities(from, to, None, None).await?;
+    let activity = activities.iter().find(|a| a.id == activity.unwrap_or(-1));
+
+    let activity = if let Some(a) = activity {
+        a
+    } else {
+        let activity_index = utils::render_list_select(
+            &activities,
+            vec!["Index", "Date", "Duration", "Project", "Task", "Description"],
+            "Choose your Acitivity: ",
+            &(|(index, activity)| {
+                vec![
+                    index.to_string(),
+                    activity.date.clone(),
+                    activity.hours.to_string(),
+                    activity.project.name.clone(),
+                    activity.task.name.clone(),
+                    activity.description
+                        .as_ref()
+                        .unwrap_or(&String::new())
+                        .to_string()
+                ]
+            }),
+        )?;
+
+        &activities[activity_index]
+    };
+
+    Ok(activity.clone())
+}
