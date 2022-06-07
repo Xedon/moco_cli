13ee9d750ff2b873b648300d4f1f03b4d7ee4f86
Rodolfo Kirch Veiga <rodolfoksveiga@gmail.com>
2022-06-07 19:50:20 +0200

    move promp functions to utils

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use crate::{
 use chrono::Utc;
 use jira_tempo::client::JiraTempoClient;
 use log::trace;
-use utils::render_table;
+use utils::{render_table, promp_task_select, promp_activitie_select};
 
 use crate::moco::model::{CreateActivitie, GetActivitie, ControlActivitieTimer};
 
@@ -347,102 +347,4 @@ async fn main() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
-}
-
-async fn promp_task_select(
-    moco_client: &MocoClient,
-    project: Option<i64>,
-    task: Option<i64>,
-) -> Result<(moco::model::Project, moco::model::ProjectTask), Box<dyn Error>> {
-    let projects = moco_client.get_assigned_projects().await?;
-    let project = projects.iter().find(|p| p.id == project.unwrap_or(-1));
-
-    let project = if let Some(p) = project {
-        p
-    } else {
-        let project_index = utils::render_list_select(
-            &projects,
-            vec!["Index", "Customer", "Project", "Project ID"],
-            "Chose your Project: ",
-            &(|(index, project)| {
-                vec![
-                    index.to_string(),
-                    project.customer.name.clone(),
-                    project.name.clone(),
-                    project.id.to_string(),
-                ]
-            }),
-        )?;
-
-        &projects[project_index]
-    };
-
-    let task = project.tasks.iter().find(|t| t.id == task.unwrap_or(-1));
-
-    let task = if let Some(t) = task {
-        t
-    } else {
-        let task_index = utils::render_list_select(
-            &project.tasks,
-            vec!["Index", "Task", "Task ID"],
-            "Chose your Task: ",
-            &(|(index, task)| vec![index.to_string(), task.name.clone(), task.id.to_string()]),
-        )?;
-        &project.tasks[task_index]
-    };
-
-    Ok((project.clone(), task.clone()))
-}
-
-async fn promp_activitie_select(
-    moco_client: &MocoClient,
-    activity: Option<i64>,
-) -> Result<moco::model::Activitie, Box<dyn Error>> {
-    let now = Utc::now().format("%Y-%m-%d").to_string();
-
-    print!("List activities from (YYYY-MM-DD) - Default 'today': ");
-    std::io::stdout().flush()?;
-
-    let mut from = utils::read_line()?;
-    if from.is_empty() {
-        from = now.clone();
-    }
-
-    print!("List activities to (YYYY-MM-DD) - Default 'last answer': ");
-    std::io::stdout().flush()?;
-
-    let mut to = utils::read_line()?;
-    if to.is_empty() {
-        to = from.clone();
-    }
-
-    let activities = moco_client.get_activities(from, to, None, None).await?;
-    let activity = activities.iter().find(|a| a.id == activity.unwrap_or(-1));
-
-    let activity = if let Some(a) = activity {
-        a
-    } else {
-        let activity_index = utils::render_list_select(
-            &activities,
-            vec!["Index", "Date", "Duration", "Project", "Task", "Description"],
-            "Choose your Acitivity: ",
-            &(|(index, activity)| {
-                vec![
-                    index.to_string(),
-                    activity.date.clone(),
-                    activity.hours.to_string(),
-                    activity.project.name.clone(),
-                    activity.task.name.clone(),
-                    activity.description
-                        .as_ref()
-                        .unwrap_or(&String::new())
-                        .to_string()
-                ]
-            }),
-        )?;
-
-        &activities[activity_index]
-    };
-
-    Ok(activity.clone())
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,7 @@
-use std::{error::Error, io::Write};
+use std::{error::Error, io::Write, vec};
+
+use crate::moco::model::{Activitie, Project, ProjectTask};
+use crate::moco::client::MocoClient;
 
 use chrono::Utc;
 
@@ -119,4 +122,102 @@ pub fn mandatory_validator(input: &str) -> Option<String> {
     } else {
         None
     }
+}
+
+pub async fn promp_task_select(
+    moco_client: &MocoClient,
+    project: Option<i64>,
+    task: Option<i64>,
+) -> Result<(Project, ProjectTask), Box<dyn Error>> {
+    let projects = moco_client.get_assigned_projects().await?;
+    let project = projects.iter().find(|p| p.id == project.unwrap_or(-1));
+
+    let project = if let Some(p) = project {
+        p
+    } else {
+        let project_index = render_list_select(
+            &projects,
+            vec!["Index", "Customer", "Project", "Project ID"],
+            "Chose your Project: ",
+            &(|(index, project)| {
+                vec![
+                    index.to_string(),
+                    project.customer.name.clone(),
+                    project.name.clone(),
+                    project.id.to_string(),
+                ]
+            }),
+        )?;
+
+        &projects[project_index]
+    };
+
+    let task = project.tasks.iter().find(|t| t.id == task.unwrap_or(-1));
+
+    let task = if let Some(t) = task {
+        t
+    } else {
+        let task_index = render_list_select(
+            &project.tasks,
+            vec!["Index", "Task", "Task ID"],
+            "Chose your Task: ",
+            &(|(index, task)| vec![index.to_string(), task.name.clone(), task.id.to_string()]),
+        )?;
+        &project.tasks[task_index]
+    };
+
+    Ok((project.clone(), task.clone()))
+}
+
+pub async fn promp_activitie_select(
+    moco_client: &MocoClient,
+    activity: Option<i64>,
+) -> Result<Activitie, Box<dyn Error>> {
+    let now = Utc::now().format("%Y-%m-%d").to_string();
+
+    print!("List activities from (YYYY-MM-DD) - Default 'today': ");
+    std::io::stdout().flush()?;
+
+    let mut from = read_line()?;
+    if from.is_empty() {
+        from = now.clone();
+    }
+
+    print!("List activities to (YYYY-MM-DD) - Default 'last answer': ");
+    std::io::stdout().flush()?;
+
+    let mut to = read_line()?;
+    if to.is_empty() {
+        to = from.clone();
+    }
+
+    let activities = moco_client.get_activities(from, to, None, None).await?;
+    let activity = activities.iter().find(|a| a.id == activity.unwrap_or(-1));
+
+    let activity = if let Some(a) = activity {
+        a
+    } else {
+        let activity_index = render_list_select(
+            &activities,
+            vec!["Index", "Date", "Duration", "Project", "Task", "Description"],
+            "Choose your Acitivity: ",
+            &(|(index, activity)| {
+                vec![
+                    index.to_string(),
+                    activity.date.clone(),
+                    activity.hours.to_string(),
+                    activity.project.name.clone(),
+                    activity.task.name.clone(),
+                    activity.description
+                        .as_ref()
+                        .unwrap_or(&String::new())
+                        .to_string()
+                ]
+            }),
+        )?;
+
+        &activities[activity_index]
+    };
+
+    Ok(activity.clone())
 }
